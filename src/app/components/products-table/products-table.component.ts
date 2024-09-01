949f966a6c70482a7778b46f8749c85a3883cbf6
Berdy <berdy@off-centered.com>
2024-09-01 13:28:09 -0400

    display table of products

--- src/app/components/products-table/products-table.component.ts
+++ src/app/components/products-table/products-table.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import {ProductsService} from '../../services/products.service';
+import {AsyncPipe, NgForOf, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-products-table',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf,
+    AsyncPipe,
+    NgForOf
+  ],
   templateUrl: './products-table.component.html',
   styleUrl: './products-table.component.scss'
 })
 export class ProductsTableComponent {
 
+  constructor(public productsService: ProductsService) {
+  }
 }
