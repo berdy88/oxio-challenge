f9731f40ff30dc5669f39633e7afa3f0012c2421
Berdy <berdy@off-centered.com>
2024-09-01 12:54:37 -0400

    add routing to pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
 import {DashboardComponent} from './components/dashboard/dashboard.component';
+import {ProductsTableComponent} from './components/products-table/products-table.component';
+import {ProductsVisualizationComponent} from './components/products-visualization/products-visualization.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
-  {path: 'dashboard', component: DashboardComponent}
+  {path: 'dashboard', component: DashboardComponent, children: [
+      {path: 'table', component: ProductsTableComponent},
+      {path: 'visualization', component: ProductsVisualizationComponent}
+    ]}
 ];
